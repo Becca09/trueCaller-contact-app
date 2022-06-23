aa9d136d8942999c716ad6b3974aff17d009503d
becca09 <adeolaadefuye@gmail.com>
2022-06-23 21:20:54 +0100

    First Maven Project

--- src/main/java/africa/semicolon/trueCaller/data/models/Contact.java
+++ src/main/java/africa/semicolon/trueCaller/data/models/Contact.java
@@ -2,12 +2,14 @@ package africa.semicolon.trueCaller.data.models;
 
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Document("Contacts")
 public class Contact {
     private int id;
     @NonNull
